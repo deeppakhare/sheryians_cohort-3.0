65ff9ce42d15f7c3d20a3d0076f04c9996e98d55
justforit4102 <deeppakhare19@gmail.com>
2026-04-23 15:36:22 +0530

    while loop question

--- DSA/Introduction to loop/sunOfDigit.js
+++ DSA/Introduction to loop/sunOfDigit.js
@@ -13,25 +13,6 @@ console.log(sum);
 
 //  --------------------------- 
 
-let prompt = require('prompt-sync')();
-let num = Number(prompt("Enter the number to check Automorphic:- "));
-let temp = num;
-let square = num * num;
-let isAuto = true;
-while(temp > 0){
-    if(temp % 10 !== square % 10){
-        isAuto = false;
-        break;
-    }
-    
-    temp = Math.floor(temp / 10);
-    square = Math.floor(num / 10);
-}
-if(isAuto){
-    console.log("It is Automorphic");
-} else {
-    console.log("It is not Automorphic");
-}
 
 
 
