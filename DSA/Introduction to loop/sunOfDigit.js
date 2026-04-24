290ec07c13e3bcc05d4127ce4c54402b667fc92c
justforit4102 <deeppakhare19@gmail.com>
2026-04-24 19:35:14 +0530

    while loop question

--- DSA/Introduction to loop/sunOfDigit.js
+++ DSA/Introduction to loop/sunOfDigit.js
@@ -3,17 +3,7 @@ let num = Number(prompt("Enter Digit :- "));
 let sum = 0;
 while(num > 0){
     let digit = num % 10;
-    // console.log(Math.floor(digit));
-    // process.stdout.write(digit + " ");
     sum += digit
     num = Math.floor(num / 10);
 }
 console.log(sum);
-
-
-//  --------------------------- 
-
-
-
-
-// ---------------------------------------------------
