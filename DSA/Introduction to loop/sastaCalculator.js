42c4ae1c9d6455e8ffa1bcf106537cda102b8576
justforit4102 <deeppakhare19@gmail.com>
2026-04-26 15:12:13 +0530

    Question practice

--- DSA/Introduction to loop/sastaCalculator.js
+++ DSA/Introduction to loop/sastaCalculator.js
@@ -7,30 +7,35 @@ do {
   console.log("Enter 4 for Division:- ");
   num = Number(prompt("Enter the Number:- "));
   switch (num) {
-    case 1:
+    case 1: {
       let a = Number(prompt("Enter first nnumber:- "));
       let b = Number(prompt("Enter second number:- "));
       console.log("Addition is " + (a + b));
       break;
-    case 2:
-      a = Number(prompt("Enter first nnumber:- "));
-      b = Number(prompt("Enter second number:- "));
+    }
+    case 2: {
+      let a = Number(prompt("Enter first nnumber:- "));
+      let b = Number(prompt("Enter second number:- "));
       console.log("Substraction is " + (a - b));
       break;
-    case 3:
+    }
+    case 3: {
       a = Number(prompt("Enter first nnumber:- "));
       b = Number(prompt("Enter second number:- "));
       console.log("Multiplication is " + a * b);
       break;
-    case 4:
+    }
+    case 4: {
       a = Number(prompt("Enter first nnumber:- "));
       b = Number(prompt("Enter second number:- "));
       console.log("Division is " + Math.floor(a / b));
       break;
+    }
 
-    default:
+    default: {
       console.log("Invalid Number");
       break;
+    }
   }
   num = Number(prompt("Enter 10 for restart:- "));
 } while (num == 10);
