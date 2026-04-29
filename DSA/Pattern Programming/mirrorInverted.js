aa330aa34e409fd5c2235706eeb0bb87ad8f8eb1
justforit4102 <deeppakhare19@gmail.com>
2026-04-29 20:39:48 +0530

    pattern programming

---
+++ DSA/Pattern Programming/mirrorInverted.js
@@ -0,0 +1,12 @@
+let n = 5;
+for(let i = 1; i <= n; i++){
+    for(let j = 1; j <= n - i + 1; j++){
+        process.stdout.write("_ ");
+        if(j == n) {
+            process.stdout.write("* ");
+            n--
+        }
+    }
+    console.log( );
+    
+}
