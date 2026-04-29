f3a0932da43c68a03d58fa3dbe7b1f669dce3ed5
justforit4102 <deeppakhare19@gmail.com>
2026-04-29 20:10:11 +0530

    pattern programming

---
+++ DSA/Pattern Programming/invertedRight.js
@@ -0,0 +1,7 @@
+let n = 5;
+for(let i = 1; i <= n; i++){
+        for(let j = 1; j <= n - i + 1; j++){
+            process.stdout.write("* ")
+        }
+        console.log( )
+    }
--- DSA/Pattern Programming/rightAngle.js
+++ DSA/Pattern Programming/rightAngle.js
@@ -1,13 +1,12 @@
-// let prompt = require('prompt-sync')();
-// let num = Number(prompt("ENter number :- "));
+let prompt = require('prompt-sync')();
+let num = Number(prompt("ENter number :- "));
 
-// for(let i = 1; i <= num; i++){
-//     for(let j = 1; j <= i; j++){
-//         process.stdout.write("* ")
-//     }
-//     console.log();
-
-// }
+for(let i = 1; i <= num; i++){
+    for(let j = 1; j <= i; j++){
+        process.stdout.write("* ")
+    }
+    console.log();
+}
 
 // let prompt = require('prompt-sync')();
 // let num = Number(prompt("ENter number :- "));
@@ -20,17 +19,17 @@
 
 // }
 
-let prompt = require("prompt-sync")();
+// let prompt = require("prompt-sync")();
 
-let num = Number(prompt("ENter number between 65 to 90:- "));
+// let num = Number(prompt("ENter number between 65 to 90:- "));
 
-if (num < 65 && num > 90) {
-  console.log("invalid number");
-} else {
-  for (let i = 65; i <= num; i++) {
-    for (let j = 65; j <= i; j++) {
-      process.stdout.write(String.fromCharCode(j) + " ");
-    }
-    console.log();
-  }
-}
+// if (num < 65 && num > 90) {
+//   console.log("invalid number");
+// } else {
+//   for (let i = 65; i <= num; i++) {
+//     for (let j = 65; j <= i; j++) {
+//       process.stdout.write(String.fromCharCode(j) + " ");
+//     }
+//     console.log();
+//   }
+// }
