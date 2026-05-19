2062ad021475650b034b8d7d077f5bb298ffb850
justforit4102 <deeppakhare19@gmail.com>
2026-05-19 22:05:32 +0530

    JS phase 1

---
+++ JS/Phase 1/script.js
@@ -0,0 +1,7 @@
+var chemistry = Number(prompt("Enter chem marks"));
+var math = Number(prompt("Enter math marks"));
+var physic = Number(prompt("Enter phy marks"));
+
+let average = (chemistry + math + physic) / 2;
+
+console.log(average);
