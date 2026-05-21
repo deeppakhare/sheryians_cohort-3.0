dab9d678d9ddd0b6822c72715d70c8c5cd135e3d
justforit4102 <deeppakhare19@gmail.com>
2026-05-22 01:05:55 +0530

    live session notes

--- JS/Phase 1/script.js
+++ JS/Phase 1/script.js
@@ -1,7 +1,27 @@
-var chemistry = Number(prompt("Enter chem marks"));
-var math = Number(prompt("Enter math marks"));
-var physic = Number(prompt("Enter phy marks"));
+// var chemistry = Number(prompt("Enter chem marks"));
+// var math = Number(prompt("Enter math marks"));
+// var physic = Number(prompt("Enter phy marks"));
 
-let average = (chemistry + math + physic) / 2;
+// let average = (chemistry + math + physic) / 2;
 
-console.log(average);
+// console.log(average);
+
+
+//  Print table from user input
+
+// var num = Number(prompt("Enter Number"));
+// var a = 1;
+// while(a <=10){
+//     console.log(`${num} * ${a} = ${num * a}`);
+//     a++;
+// }
+
+
+// user se password pucho
+
+
+do{
+    var pass = prompt("Enter the password");
+} while(pass != '123')
+
+console.log("Welcome");
