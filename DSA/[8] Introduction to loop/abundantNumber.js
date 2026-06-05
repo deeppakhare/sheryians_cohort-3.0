ee7d5c0140ebb6460926a4b9d5fc6c356816856a
justforit4102 <deeppakhare19@gmail.com>
2026-06-05 23:59:16 +0530

    erf

--- DSA/[8] Introduction to loop/abundantNumber.js
+++ DSA/[8] Introduction to loop/abundantNumber.js
@@ -1,9 +1,49 @@
-let nStr = 12;
+// let nStr = 12;
+// let sum = 0;
+// for (let i = 1; i < nStr; i++) {
+//   if (nStr % i == 0) {
+//     sum += i;
+//   }
+// }
+// if (sum > nStr) console.log("Yes");
+// else console.log("No");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let num = 55;
 let sum = 0;
-for (let i = 1; i < nStr; i++) {
-  if (nStr % i == 0) {
-    sum += i;
-  }
+for(let i = 1; i <= num/2; i++){
+  if(num % i == 0) sum += i;
+  
 }
-if (sum > nStr) console.log("Yes");
-else console.log("No");
+console.log(sum);
+
+if(sum > num) console.log("it is abundant");
+else console.log("It is not abundant");
+
