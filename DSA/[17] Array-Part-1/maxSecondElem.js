02ee0120af64a5279c8b7bc6a9d4e1a2c3c8b5ed
justforit4102 <deeppakhare19@gmail.com>
2026-06-09 20:46:04 +0530

    array dsa ques.

--- DSA/[17] Array-Part-1/maxSecondElem.js
+++ DSA/[17] Array-Part-1/maxSecondElem.js
@@ -1,5 +1,7 @@
-let arr = [23, 14, 56, 87, 64, 90, 27];
+let arr = [2, 96, 69, 77, 145, 20];
 // let arr = [3, 27, 27, 27, 27, 27, 27];
+
+
 
 let first = Math.max(arr[0], arr[1]);
 let second = Math.min(arr[0], arr[1]);
@@ -8,7 +10,7 @@ for (let i = 2; i < arr.length; i++) {
   if (arr[i] > first) {
     second = first;
     first = arr[i];
-  } else if (arr[i] > second && first != arr[i]) {
+  } else if ((arr[i] > second && arr[i] < first) && arr[i] != first) {
     second = arr[i];
   }
 }
---
+++ DSA/[17] Array-Part-1/mean.js
@@ -0,0 +1,8 @@
+// Calculate the sum and mean of the array elements
+
+let n = 5;
+let arr = new Array(n);
+let prompt = require('prompt-sync')();
+let num = prompt("Enter number for array");
+
+console.log(arr);
