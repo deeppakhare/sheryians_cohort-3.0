bb5a149c3cdb4a8f4eaf068ec041774caf7135a3
justforit4102 <deeppakhare19@gmail.com>
2026-06-11 17:39:24 +0530

    array question

--- DSA/[18] Array-Part-2/rotateRight.js
+++ DSA/[18] Array-Part-2/rotateRight.js
@@ -1,8 +1,8 @@
 let arr = [1,2,3,4,5];
-let temp = arr[0];
-for(let i = arr.length-2;i < arr.length; i--){
-    arr[i] = arr[i+1]
+let temp = arr[arr.length-1];
+for(let i = arr.length-1;i >= 1; i--){
+    arr[i] = arr[i-1]
 }
-arr[arr.length-1] = temp
+arr[0] = temp
 console.log(arr);
 
