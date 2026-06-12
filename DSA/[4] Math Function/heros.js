306d5544d1db777837aa50056e235394c8b3d587
justforit4102 <deeppakhare19@gmail.com>
2026-06-12 23:04:11 +0530

    math function

--- DSA/[4] Math Function/heros.js
+++ DSA/[4] Math Function/heros.js
@@ -4,4 +4,10 @@ let a = Number(prompt("Enter first side: "));
 let b = Number(prompt("Enter second side: "));
 let c = Number(prompt("Enter third side: "));
 
+// let first find semi-perimeter
+let s = (a + b + c) / 2;
 
+// Let find Area
+let area = Math.sqrt(s * (s - a) * (s - b) * (s - c));
+
+console.log(area);
