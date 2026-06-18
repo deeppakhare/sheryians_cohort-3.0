bdfdf4c29ccdb1782ba0512f5a4e7e0be57211ae
justforit4102 <deeppakhare19@gmail.com>
2026-06-18 23:53:47 +0530

    wrong file

--- JS/Phase 7 (DOM)/Day5/script.js
+++ JS/Phase 7 (DOM)/Day5/script.js
@@ -1,147 +1,73 @@
-const createBtn = document.querySelector("#create");
-const formDiv = document.querySelector(".form");
-const closeBtn = document.querySelector("#close");
+// const five = document.querySelector("#five");
+// const main = document.querySelector("main");
 
-const productDiv = document.querySelector(".products");
+// five.addEventListener("click", (event) => {
+//   console.log(event.target);
+// });
 
-const form = document.querySelector("form");
+// main.addEventListener("click", (e) => {
+//   console.log(e.target);
+// });
 
-const productsArr = JSON.parse(localStorage.getItem("products")) || [];
-console.log(productsArr);
+// let random = Math.random() * 10;
+// let r = Math.ceil(random);
 
-let updateIndex = null;
+// console.log(r);
 
-let ui = () => {
-  productDiv.innerHTML = "";
-  productsArr.forEach((elem, index) => {
-    productDiv.innerHTML += `<div class="product-card">
-          <div class="img">
-            <img
-              src="${elem.image}"
-              alt=""
-            />
-          </div>
+const main = document.querySelector("main");
+const overlay = document.querySelector("#overlay");
 
-          <div class="text">
-            <h3>${elem.productName}</h3>
-            <p>${elem.description}</p>
-            <p>${elem.price}</p>
-          </div>
+const btn = document.querySelector("button");
+const timer = document.querySelector("#timer");
+const scoree = document.querySelector("#score");
 
-          <div class="btns">
-            <button onclick="updateProduct('${elem.productName}')" id="update">Update</button>
-            <button onclick="deleteProduct(${index})" id="delete">Delete</button>
-          </div>
-        </div>`;
-  });
+const box = document.createElement("div");
+box.classList.add("box");
+
+let interval;
+let time = 0;
+let score = 0;
+
+const randomColor = () => {
+  let r = Math.floor(Math.random() * 256);
+  let g = Math.floor(Math.random() * 256);
+  let b = Math.floor(Math.random() * 256);
+
+  return `rgb(${r}, ${g}, ${b})`;
 };
 
-ui();
+const randomBox = () => {
+  box.style.backgroundColor = randomColor();
+  main.append(box);
 
-createBtn.addEventListener("click", () => {
-  formDiv.style.display = "flex";
-});
+  let mainH = main.clientHeight - box.offsetHeight;
+  let mainW = main.clientWidth - box.offsetWidth;
 
-closeBtn.addEventListener("click", () => {
-  formDiv.style.display = "none";
-});
+  const rY = Math.random() * mainH;
+  const rX = Math.random() * mainW;
 
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
-
-  let productName = event.target[0].value;
-  let description = event.target[1].value;
-  let price = event.target[2].value;
-  let image = event.target[3].value;
-
-  if (
-    productName.trim() === "" ||
-    description.trim() === "" ||
-    price.trim() === "" ||
-    image === ""
-  ) {
-    alert("please fill all the fields");
-    return;
-  }
-
-  let obj = {
-    productName,
-    description,
-    price,
-    image,
-  };
-
-  if (updateIndex !== null) {
-    productsArr[updateIndex] = obj;
-    updateIndex = null;
-    localStorage.setItem("products", JSON.stringify(productsArr));
-  } else {
-    productsArr.push(obj);
-    localStorage.setItem("products", JSON.stringify(productsArr));
-  }
-
-  ui();
-  console.log(productsArr);
-
-  form.reset();
-
-  formDiv.style.display = "none";
-});
-
-const updateProduct = (name) => {
-  formDiv.style.display = "flex";
-  let product = productsArr.find((elem) => elem.productName === name);
-  updateIndex = productsArr.findIndex((elem) => elem.productName === name);
-
-  form[0].value = product.productName;
-  form[1].value = product.description;
-  form[2].value = product.price;
-  form[3].value = product.image;
+  box.style.top = `${rY}px`;
+  box.style.left = `${rX}px`;
 };
 
-const deleteProduct = (index) => {
-  productsArr.splice(index, 1);
-  localStorage.setItem("products", JSON.stringify(productsArr));
-  ui();
-};
+btn.addEventListener("click", () => {
+  clearInterval(interval);
 
-// localStorage.setItem("name", "Nitin gadkari");
+  interval = setInterval(() => {
+    randomBox();
+    time += 1;
+    timer.textContent = time;
+  }, 1000);
 
-// localStorage.setItem("song", "tera mera khatam");
+  setTimeout(() => {
+    clearInterval(interval);
+    overlay.style.display = "flex";
+  }, 10000);
+});
 
-// let lsd = localStorage.getItem("name");
+box.addEventListener("click", () => {
+  score += 1;
+  scoree.textContent = score;
+});
 
-// let song = localStorage.getItem("song");
-// console.log(lsd);
-// console.log("song->", song);
 
-// let data = [
-//   {
-//     name: "piyush",
-//     age: 69,
-//     address: "saket nagar",
-//     pincode: 462022,
-//   },
-//   {
-//     name: "Aryan kelvin",
-//     age: 69,
-//     address: "saket nagar",
-//     pincode: 462022,
-//   },
-//   {
-//     name: "Bhuvan bam",
-//     age: 69,
-//     address: "Mumbai",
-//     pincode: 462022,
-//   },
-// ];
-
-// let newData = ["polo"];
-
-// localStorage.setItem("fam-people", JSON.stringify(data));
-
-// const lsd = localStorage.getItem("fam-people");
-
-// let value = JSON.parse(lsd);
-
-// console.log(value);
