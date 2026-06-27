b55cd0002d07a814b14505847d29027c031d2b3a
justforit4102 <deeppakhare19@gmail.com>
2026-06-27 21:29:36 +0530

    dsa question

--- DSA/[18] Array-Part-2/rotateLeft.js
+++ DSA/[18] Array-Part-2/rotateLeft.js
@@ -9,3 +9,22 @@ console.log(arr);
 
 
 
+if (s.length !== t.length) return false;
+    
+    const count = {};
+    
+    // Count frequencies in string s
+    for (let char of s) {
+        count[char] = (count[char] || 0) + 1;
+    }
+    
+    // Decrement frequencies using string t
+    for (let char of t) {
+        if (!count[char]) {
+            // Char missing or count falls below zero
+            return false;
+        }
+        count[char]--;
+    }
+    
+    return true;
