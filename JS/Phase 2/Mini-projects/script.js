7d737a3a345689b62cb3a9cb85998dbd42efb6b9
justforit4102 <deeppakhare19@gmail.com>
2026-07-12 23:29:59 +0530

    librabry book logic

--- JS/Phase 2/Mini-projects/script.js
+++ JS/Phase 2/Mini-projects/script.js
@@ -1,1 +1,25 @@
-console.log("JS practice");
+// ### Project 5: Library Management System
+
+// Combines arrays, objects, and functions beautifully.
+
+let library = {
+    books: [],
+
+    addbook(title, author) {
+        let check = this.books.find(name => name.title === title);      
+        
+        if (check) {
+            return 'It is alredy available'
+        }
+        this.books.push({
+            id: this.books.length + 1,
+            title,
+            author,
+            isBorrowd: false
+        });
+        return "Book added successfully"
+    }
+};
+library.addbook("Rich", "Deep");
+console.log(library.addbook("Rich1", "Deep"));
+console.log(library.books);
