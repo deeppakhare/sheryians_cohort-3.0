46b848fb2ec1b3ef6fd69571d989784cc8de1287
justforit4102 <deeppakhare19@gmail.com>
2026-07-13 13:52:45 +0530

    word frequency counter

--- JS/Phase 2/Mini-projects/script.js
+++ JS/Phase 2/Mini-projects/script.js
@@ -1,62 +1,80 @@
+
+// ### Project 4: Word Frequency Counter
+
+
+// function frequency(text) {
+//     let words = text.toLowerCase().split(/\s+/);
+//     let freq = {};
+//     words.forEach(word => {
+//         freq[word] = (freq[word] || 0) + 1;
+//     })
+//     return freq;
+// }
+
+// let text = "The quick brown fox jumps Over the lazy dog the fox is Quick"
+// console.log(frequency(text));
+
+
+
 // ### Project 5: Library Management System
 
 // Combines arrays, objects, and functions beautifully.
 
-let library = {
-    books: [],
+// let library = {
+//     books: [],
 
-    addBook(title, author) {
-        let check = this.books.find(name => name.title === title);      
-        if (check) {
-            return 'It is alredy available'
-        }
-        this.books.push({
-            id: this.books.length + 1,
-            title,
-            author,
-            isBorrowd: false
-        });
-        return `Book added successfully `
-    },
+//     addBook(title, author) {
+//         let check = this.books.find(name => name.title === title);      
+//         if (check) {
+//             return 'It is alredy available'
+//         }
+//         this.books.push({
+//             id: this.books.length + 1,
+//             title,
+//             author,
+//             isBorrowd: false
+//         });
+//         return `Book added successfully `
+//     },
 
-    borrowBook(id){
-        let checkId = this.books.find(n => n.id === id)
+//     borrowBook(id){
+//         let checkId = this.books.find(n => n.id === id)
         
-        if(!checkId) return " book  Not found";
+//         if(!checkId) return " book  Not found";
 
-        if(checkId.isBorrowd) return "Already borrowed"
-        checkId.isBorrowd = true;
-        return "Borrowed success"
-    },
+//         if(checkId.isBorrowd) return "Already borrowed"
+//         checkId.isBorrowd = true;
+//         return "Borrowed success"
+//     },
 
-    returnBook(id){
-        let checkId = this.books.find(n => n.id === id)
+//     returnBook(id){
+//         let checkId = this.books.find(n => n.id === id)
 
-        if(!checkId) return " book Not found";
+//         if(!checkId) return " book Not found";
 
-        if(checkId.isBorrowd == false) return "wrond id"
-        checkId.isBorrowd = false;
-        return "Book Returned"
-    },
+//         if(checkId.isBorrowd == false) return "wrond id"
+//         checkId.isBorrowd = false;
+//         return "Book Returned"
+//     },
 
-    availableBooks() {
-        return this.books.filter((n) => n.isBorrowd === false);
-    }
-};
-console.log(library.addBook("Rich1", "Deep"));
-console.log(library.addBook("Rich2", "Deep"));
-console.log(library.addBook("Rich3", "Deep"));
-console.log(library.addBook("Rich4", "Deep"));
-
-
-console.log(library.borrowBook(2));
-console.log(library.borrowBook(4));
-
-console.log(library.books);
-
-console.log(library.returnBook(2));
-
-console.log(library.books);
+//     availableBooks() {
+//         return this.books.filter((n) => n.isBorrowd === false);
+//     }
+// };
+// console.log(library.addBook("Rich1", "Deep"));
+// console.log(library.addBook("Rich2", "Deep"));
+// console.log(library.addBook("Rich3", "Deep"));
+// console.log(library.addBook("Rich4", "Deep"));
 
 
-console.log(library.availableBooks());
+// console.log(library.borrowBook(2));
+// console.log(library.borrowBook(4));
+
+// console.log(library.books);
+
+// console.log(library.returnBook(2));
+
+// console.log(library.books);
+
+
+// console.log(library.availableBooks());
