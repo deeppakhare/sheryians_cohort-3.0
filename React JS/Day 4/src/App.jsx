514231045dfe0e3825a127b7a85638fad47066ff
justforit4102 <deeppakhare19@gmail.com>
2026-07-27 14:25:34 +0530

    react practice

---
+++ React JS/Day 4/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Contact from './Contact'
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello</h1>
+      <Contact/>
+    </div>
+  )
+}
+
+export default App
