e276e820fa364285e38e88186eda185a65253c0b
justforit4102 <deeppakhare19@gmail.com>
2026-07-30 01:50:00 +0530

    day 5 react

--- React JS/Day 5/src/App.jsx
+++ React JS/Day 5/src/App.jsx
@@ -1,9 +1,10 @@
+
 import React from 'react'
 
 const App = () => {
   return (
     <div>
-      <h1>Hello</h1>
+      <h1 className='text-red-600 text-9xl font-bold font-serif flex align-middle justify-center underline'>Hello</h1>
     </div>
   )
 }
