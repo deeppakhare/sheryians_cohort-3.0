7c29212cea70b39a91b007b675f2b6bb4a0b3bca
justforit4102 <deeppakhare19@gmail.com>
2026-07-30 14:19:53 +0530

    products fetching

--- React JS/Day 5/src/ProductCard.jsx
+++ React JS/Day 5/src/ProductCard.jsx
@@ -3,12 +3,12 @@ import React from 'react'
 const ProductCard = () => {
   return (
     <div>
-      <div >
-        <img src="https://imgs.search.brave.com/pv8oepH2hyfG0LUmeOgMNBgG8M8p_Q8ramCymcy-hFw/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9ibG9n/LmxvZ3JvY2tldC5j/b20vd3AtY29udGVu/dC91cGxvYWRzLzIw/MTgvMDcvY3NzLXRy/YW5zaXRpb25zLWFu/aW1hdGUtaGFtYnVy/Z2VyLWJ1dHRvbi5w/bmc" alt="" />
+      <div className='text-white border-2 p-5 flex flex-col w-70 h-100 gap-5 bg-blue-300'>
+        <img className='w-60' src="https://imgs.search.brave.com/pv8oepH2hyfG0LUmeOgMNBgG8M8p_Q8ramCymcy-hFw/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9ibG9n/LmxvZ3JvY2tldC5j/b20vd3AtY29udGVu/dC91cGxvYWRzLzIw/MTgvMDcvY3NzLXRy/YW5zaXRpb25zLWFu/aW1hdGUtaGFtYnVy/Z2VyLWJ1dHRvbi5w/bmc" alt="" />
         <h1>Name od Product</h1>
-        <p>Description</p>
-        <p>Price</p>
-        <button>Delete</button>
+        <p className='text-[13px]'>Description</p>
+        <p className='text-green-600'>Price</p>
+        <button className='bg-amber-300 p-3 text-[20px] font-bold font-mono'>Delete</button>
       </div>
     </div>
   )
