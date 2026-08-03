2b7f761c426565a748dd8276ad42a8af77458245
justforit4102 <deeppakhare19@gmail.com>
2026-08-03 14:56:48 +0530

    day 6

---
+++ React JS/Day 6/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='w-screen h-screen bg-amber-600 flex justify-center items-center border-50 border-amber-50'>
+      <div className=''>
+
+      </div>
+    </div>
+  )
+}
+
+export default App
