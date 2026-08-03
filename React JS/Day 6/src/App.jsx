8193dc855af43a3cddfc975e5d9c1c4175643d3a
justforit4102 <deeppakhare19@gmail.com>
2026-08-03 15:51:05 +0530

    form handling better approach type

--- React JS/Day 6/src/App.jsx
+++ React JS/Day 6/src/App.jsx
@@ -2,21 +2,29 @@ import React from 'react'
 import { useState } from 'react'
 
 const App = () => {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("")
-  const [password, setPassword] = useState("")
+  const [formdata, setFormdata] = useState({
+    name: "",
+    email: "",
+    password: ""
+  })
+
+  // const handelChange = (e) => {
+  //   setFormdata(...formdata, name: e.target.value);
+  // }
+  // console.log(formdata);
+  
   return (
     <div className='w-screen h-screen bg-amber-600 flex justify-center items-center border-50 border-amber-50'>
       <div className='h-[70%] w-[70%] bg-blue-400 flex flex-col justify-evenly items-center'>
           <div className='flex flex-col justify-evenly h-[50%]'>
-            <input onChange={(e) => setName(e.target.value)} type="text" placeholder='Enter name'  className='border-2'/>
-            <input onChange={(e) => setEmail(e.target.value)} type="text" placeholder='Enter name'  className='border-2'/>
-            <input onChange={(e) => setPassword(e.target.value)} type="text" placeholder='Enter name'  className='border-2'/>
+            <input name='name' onChange={(e) => setFormdata({...formdata,name:e.target.value})} type="text" placeholder='Enter name'  className='border-2'/>
+            <input name='email' onChange={(e) => setFormdata({...formdata,email:e.target.value})} type="text" placeholder='Enter name'  className='border-2'/>
+            <input name='password' onChange={(e) => setFormdata({...formdata,password:e.target.value})} type="text" placeholder='Enter name'  className='border-2'/>
           </div>
           <div className='h-[50%] flex flex-col justify-evenly'>
-            <h1>Name is - {name}</h1>
-            <h1>Email is - {email}</h1>
-            <h1>password is - {password}</h1>
+            <h1>Name is - {formdata.name}</h1>
+            <h1>Email is - {formdata.email}</h1>
+            <h1>password is - {formdata.password}</h1>
           </div>
       </div>
     </div>
