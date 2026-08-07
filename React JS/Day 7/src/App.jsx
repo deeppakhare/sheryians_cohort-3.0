bf048ac82b0ffbedacf96a7ca256f1bd38b09eee
justforit4102 <deeppakhare19@gmail.com>
2026-08-07 23:23:16 +0530

    day 7

--- React JS/Day 7/src/App.jsx
+++ React JS/Day 7/src/App.jsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from "react";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import Usercard from "./components/Usercard";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [toggle, setToggle] = useState(false);
+  const [users, setUsers] = useState([]);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className="bg-gray-300 h-screen">
+      <Register setUsers={setUsers} setToggle={setToggle} />
 
-export default App
+      <div className="flex gap-4">
+        {users.map((elem) => (
+          <Usercard user={elem} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default App;
