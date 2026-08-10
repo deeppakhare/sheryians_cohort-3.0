a957a66bb952f3246793861474f295834bfb9f30
justforit4102 <deeppakhare19@gmail.com>
2026-08-10 22:14:15 +0530

    TS day2

--- Typescript/Day 2/index.js
+++ Typescript/Day 2/index.js
@@ -2,4 +2,17 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 let a = 90;
 console.log(a);
+let b = 34;
+b = 12;
+let sum = a + b;
+console.log(sum);
+// running typescript file and adding nodemon for watching error
+// Premitive Data type :- string,boolean, number, undefined,bigint,symbol
+// Array  
+// Tuples 
+// enums (basics)
+// Union Type
+// Literal Type
+let arr = [1, 33, 23, 676, true, false, "hello", undefined, null];
+console.log(arr);
 //# sourceMappingURL=index.js.map
--- Typescript/Day 2/index.ts
+++ Typescript/Day 2/index.ts
@@ -1,2 +1,21 @@
-let a = 90;
+let a: number = 90;
 console.log(a);
+
+let b: number = 34;
+
+b = 12;
+let sum = a + b;
+console.log(sum);
+
+
+// running typescript file and adding nodemon for watching error
+// Premitive Data type :- string,boolean, number, undefined,bigint,symbol
+// Array  
+// Tuples 
+// enums (basics)
+// Union Type
+// Literal Type
+
+
+let arr: any[] = [1, 33, 23, 676, true, false, "hello", undefined, null];
+console.log(arr);
