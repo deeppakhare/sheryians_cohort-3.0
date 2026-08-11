f8a922ff1191c99c946b3a64b2688f317244325f
justforit4102 <deeppakhare19@gmail.com>
2026-08-11 23:15:21 +0530

    TS day3

--- Typescript/Day 3/index.js
+++ Typescript/Day 3/index.js
@@ -1,34 +1,10 @@
 "use strict";
-// type UserObj = {
-//   name: string;
-//   age: number;
-//   company: string;
-//   address: {
-//     street: string;
-//     city: string;
-//     state: string;
-//   };
-//   employeeId?: string;
-// };
-Object.defineProperty(exports, "__esModule", { value: true });
-// let userObj: UserObj = {
-//   name: "rahul",
-//   age: 45,
-//   company: "sheryians",
-//   address: {
-//     street: "Malabar road",
-//     city: "Mumbai",
-//     state: "Maharastra",
-//   },
-// };
-// userObj.name = "pl";
-// userObj.employeeId = "spl89";
-// console.log(userObj);
 // let sum = (a: number, b: () => number): number => {
 //   console.log(a);
 //   let data = b();
 //   return a + data;
 // };
+Object.defineProperty(exports, "__esModule", { value: true });
 // // let res: number = sum(78, 78);
 // // console.log(res);
 // let val = sum(78, () => 56);
