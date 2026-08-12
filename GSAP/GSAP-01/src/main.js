ddd9803dc2b92e736dcdef8fa8fb8d9e6dd7347f
justforit4102 <deeppakhare19@gmail.com>
2026-08-12 21:20:47 +0530

    animation practice

--- GSAP/GSAP-01/src/main.js
+++ GSAP/GSAP-01/src/main.js
@@ -1,9 +1,41 @@
 import { gsap } from 'gsap'
 import './style.css'
 
-gsap.to(".box", {
-    x: 300,
-    rotation: 360,
-    duration: 2,
-    ease: "power2.out"
-});
+// gsap.to("#boxId", {
+//     x: 300,
+//     rotation: 360,
+//     duration: 2,
+//     ease: "power2.out"
+// });
+
+// gsap.to(".box", {
+//     y:500,
+//     duration:2,
+//     ease:"bounce",
+// })
+
+
+// gsap.from(".box", {
+//     y:500,
+//     duration:2,
+//     ease:"bounce",
+// })
+
+
+// gsap.fromTo(".box", {
+//     x:500,
+// },{
+//     y:200,
+//     duration:5,
+//     ease:"elastic"
+// })
+
+// gsap.set(".box", {
+//     x:500,
+// })
+
+gsap.fromTo(
+  ".box",
+  { autoAlpha: 0, x: -200 },
+  { autoAlpha: 0.5, x: 500, duration: 2 }
+);
