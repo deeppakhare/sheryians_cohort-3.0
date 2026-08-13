a460f7b7467967f0564bc291f423e8f459c6e137
justforit4102 <deeppakhare19@gmail.com>
2026-08-14 03:18:31 +0530

    react practice

--- React JS/Day 7/src/App.jsx
+++ React JS/Day 7/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Login from './components/Login'
+import Register from './components/Register'
 
 const App = () => {
   return (
     <div className='bg-gray-300 h-screen  flex flex-col justify-center items-center '>
       <Login/>
+      <Register/>
     </div>
   )
 }
--- React JS/Day 7/src/components/Register.jsx
+++ React JS/Day 7/src/components/Register.jsx
@@ -2,14 +2,18 @@ import React from 'react'
 
 const Register = () => {
   return (
-    <div className='bg-green-200 flex flex-col justify-evenly border-2 rounded-2xl w-100 h-90 gap-3 p-5'>
+    <div className='bg-green-200 flex flex-col justify-evenly border-2 rounded-2xl w-100 h-90 gap-3 p-6 text-2xl font-mono'>
+      <div className='flex justify-center items-center'>
     <h1>Register Form</h1>
-    <form className='flex flex-col gap-3 ' action="">
-        <input type="text" placeholder='Enter Email'/>
-        <input type="password" placeholder='Enter Password'/>
-        <button>Register</button>
+      </div>
+    <form className='flex flex-col gap-6 ' action="">
+        <input type="text" placeholder='Enter Email' className='border p-1'/>
+        <input type="password" placeholder='Enter Password' className='border p-1'/>
+        <button className='border rounded p-3 hover:bg-amber-400'>Login</button>
     </form>
-    <p>Dont have account <span>Sign in</span></p>
+    <div className='flex justify-center items-center text-xl'>
+    <p>Dont have account ? <span className='text-blue-400 cursor-pointer'>Log in</span></p>
+    </div>
     </div>
   )
 }
