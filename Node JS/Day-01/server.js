3f85fc837977f112f77fc8cb78a25fa18992363d
justforit4102 <deeppakhare19@gmail.com>
2026-08-14 22:13:02 +0530

    creating first server

--- Node JS/Day-01/server.js
+++ Node JS/Day-01/server.js
@@ -1,1 +1,10 @@
-console.log("hello");
+let http = require("http");
+
+let server = http.createServer((req, res) => {
+    console.log("hello i am server");
+    res.end("mene tumhari bat sunli");
+})
+
+server.listen(3000, () => {
+    console.log("server is running");
+});
