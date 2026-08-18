d66c277e0e5be8342fa89dcb37bfed135bd4bf0d
justforit4102 <deeppakhare19@gmail.com>
2026-08-18 20:47:47 +0530

    practicing react hook form

--- React JS/Day 8/src/App.jsx
+++ React JS/Day 8/src/App.jsx
@@ -1,12 +1,14 @@
 import React, { useState } from "react";
 import Form from "./components/Form";
 import Form2 from "./components/Form2";
+import NewForm from "./components/NewForm";
 
 const App = () => {
   const [toggle, setToggle] = useState(true)
   return (
     <div className="h-screen w-screen flex flex-col justify-center items-center text-amber-500">
-      {toggle? <Form toggle={setToggle}/> : <Form2 toggle={setToggle}/>}
+      {/* {toggle? <Form toggle={setToggle}/> : <Form2 toggle={setToggle}/>} */}
+      <NewForm/>
     </div>
   );
 };
