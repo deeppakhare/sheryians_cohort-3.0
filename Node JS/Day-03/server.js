8938948cecc0db6bb48b05e13cfbe9f82f0ad7b3
justforit4102 <deeppakhare19@gmail.com>
2026-08-18 22:09:49 +0530

    CRED operations

--- Node JS/Day-03/server.js
+++ Node JS/Day-03/server.js
@@ -3,26 +3,35 @@ const app = express();
 const port = 3000;
 app.use(express.json());
 
-let users = [
-    
-];
-
+let users = [];
 
 app.get("/", (req, res) => {
-    res.send(users)
-})
+  res.send(users);
+});
 app.post("/create", (req, res) => {
-    let body = req.body;
-    users.push(body);
-    res.send("user saved success");
-})
+  let body = req.body;
+  users.push(body);
+  res.send("user saved success");
+});
 
-app.delete("/delete",(req,res) => {
-    let body = req.body;
-    users.pop(body);
-    res.send("user removed");
-})
+app.delete("/delete/:id", (req, res) => {
+  let { id } = req.params;
+  let userData = users.filter((val) => val.id !== id);
+  users = userData;
+  res.send("User deleted success");
+});
+
+app.put("/update/:id", (req, res) => {
+  let { id } = req.params;
+  let { name } = req.body;
+
+  let upadatedUser = users.map((val) =>
+    val.id === id ? { ...val, name } : val,
+  );
+
+  res.send(upadatedUser);
+});
 
 app.listen(port, () => {
-    console.log(`Port running on ${port}`);
-})
+  console.log(`Port running on ${port}`);
+});
