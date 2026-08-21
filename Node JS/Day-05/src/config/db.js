62f41bada78e4ea006aa339f79b59d1259758155
justforit4102 <deeppakhare19@gmail.com>
2026-08-21 22:33:59 +0530

    creating first databe

---
+++ Node JS/Day-05/src/config/db.js
@@ -0,0 +1,14 @@
+const { default: mongoose } = require("mongoose");
+
+const connectDb = async () => {
+  try {
+    await mongoose.connect(
+      "--------------Your compass key Key -------------------",
+    );
+    console.log("mongodb connected");
+  } catch (error) {
+    console.log("error in db", error);
+  }
+};
+
+module.exports = connectDb;
