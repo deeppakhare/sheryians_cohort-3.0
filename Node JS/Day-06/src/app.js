3fbd0da593c1116f76d85d900be133bde39be84d
justforit4102 <deeppakhare19@gmail.com>
2026-08-24 21:00:00 +0530

    backend day 6

--- Node JS/Day-06/src/app.js
+++ Node JS/Day-06/src/app.js
@@ -8,5 +8,20 @@ app.get("/", (req, res) => {
   res.send("ok ");
 });
 
+app.post("/create", async(req, res) => {
+    try {
+        let {title, description} = req.body
+
+        const newNote = await NotesModel.create({
+            title,
+            description,
+          });
+
+          return res.status(201)
+    } catch (error) {
+        console.log("Error in creation",error);
+        
+    }
+})
 
 module.exports = app;
