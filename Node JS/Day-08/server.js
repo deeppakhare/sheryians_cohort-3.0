4cd5f41f561e59a95a581cd530af2537006df3a9
justforit4102 <deeppakhare19@gmail.com>
2026-08-26 22:42:05 +0530

    day 08

--- Node JS/Day-08/server.js
+++ Node JS/Day-08/server.js
@@ -1,1 +1,9 @@
-console.log("Server js ");
+
+const app = require("./src/app");
+
+let portNo = process.env.port || 4000;
+
+app.listen(portNo, () => {
+  console.log(`Server is running on port ${portNo}`);
+});
+
