617fdf8b97b886fcfbc7948b1c6806f8b4fa2336
justforit4102 <deeppakhare19@gmail.com>
2026-08-27 23:58:00 +0530

    day 9

--- Node JS/Day-09/backend/src/routes/user,routes.js
+++ Node JS/Day-09/backend/src/routes/user,routes.js
@@ -1,10 +1,9 @@
 const express = require("express");
 const { route } = require("../app");
 const uploads = require("../config/multer.congif");
-const { create } = require("../../../../Day-07/backend/src/models/notes.model");
 
 const router = express.Router()
 
-router.post("/create",uploads.single("profilepc"),create);
+router.post("/create",uploads);
 
 module.exports = router
