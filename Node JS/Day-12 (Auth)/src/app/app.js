ba22332021b7b95082aee910939b61f0227a15ce
justforit4102 <deeppakhare19@gmail.com>
2026-09-03 20:41:17 +0530

    checking endpoints

--- Node JS/Day-12 (Auth)/src/app/app.js
+++ Node JS/Day-12 (Auth)/src/app/app.js
@@ -1,6 +1,6 @@
 import express from "express";
-import jwt from "jsonwebtoken"
-import userModel from "../models/user.model.js";
+import jwt from "jsonwebtoken";
+import userModel from "../models/users.model.js";
 import { authenticate } from "../middleware/auth.middleware.js";
 import dotenv from "dotenv";
 import bcrypt from "bcryptjs";
@@ -8,95 +8,89 @@ dotenv.config();
 
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
 app.get("/api", (req, res) => {
-    res.status(200).json({
-        message: "Welcome to the authentication API"
-    })
-})
-
+  res.status(200).json({
+    message: "Welcome to the authentication API",
+  });
+});
 
 app.post("/api/auth/register", async (req, res) => {
+  const { email, name, password } = req.body;
 
-    const { email, name, password } = req.body
+  const user = await userModel.create({
+    email,
+    name,
+    password: await bcrypt.hash(password, 10),
+  });
 
-    const user = await userModel.create({
-        email, name, password: await bcrypt.hash(password, 10)
-    })
+  const token = jwt.sign(
+    {
+      id: user._id,
+    },
+    process.env.JWT_SECRET,
+  );
 
-    const token = jwt.sign(
-        {
-            id: user._id
-        },
-        process.env.JWT_SECRET
-    )
-
-    res.status(201).json({
-        message: "User Create Successfully",
-        data: {
-            user: {
-                email,
-                name,
-                id: user._id
-            },
-            token
-        }
-    })
-
-
-})
-
+  res.status(201).json({
+    message: "User Create Successfully",
+    data: {
+      user: {
+        email,
+        name,
+        id: user._id,
+      },
+      token,
+    },
+  });
+});
 
 app.get("/api/auth/me", authenticate, async (req, res) => {
+  console.log(req.user);
 
-    console.log(req.user)
-
-    res.status(200).json({
-        data: {
-            user: req.user
-        }
-    })
-
-})
-
+  res.status(200).json({
+    message:"Plese enter valid token",
+    data: {
+      user: req.user,
+    },
+  });
+});
 
 app.post("/api/auth/login", async (req, res) => {
+  /**
+   * password = Test123#
+   */
+  const { email, password } = req.body;
 
+  const user = await userModel.findOne({
+    email,
+  });
 
-    /**
-     * password = Test123#
-     */
-    const { email, password } = req.body
+  const isValidPassword = bcrypt.compare(password, user.password);
 
-    const user = await userModel.findOne({
-        email
-    })
+  if (!isValidPassword) {
+    return res.status(400).json({
+      message: "Invalid Email or Password",
+    });
+  }
 
-    const isValidPassword = bcrypt.compare(password, user.password)
+  const token = jwt.sign(
+    {
+      id: user._id,
+    },
+    process.env.JWT_SECRET,
+  );
 
-    if (!isValidPassword) {
-        return res.status(400).json({
-            message: "Invalid Email or Password"
-        })
-    }
-
-    const token = jwt.sign({
-        id: user._id
-    }, process.env.JWT_SECRET)
-
-    res.status(200).json({
-        message: "user loggedIn successfully",
-        data: {
-            user: {
-                email: user.email,
-                name: user.name
-            }
-        },
-        token
-    })
-
-})
-
+  res.status(200).json({
+    message: "user loggedIn successfully",
+    data: {
+      user: {
+        email: user.email,
+        name: user.name,
+      },
+    },
+    token,
+  });
+});
 
 export default app;
--- Node JS/Day-12 (Auth)/src/middleware/auth.middleware.js
+++ Node JS/Day-12 (Auth)/src/middleware/auth.middleware.js
@@ -1,5 +1,5 @@
 import jwt from "jsonwebtoken";
-import userModel from "../models/user.model.js";
+import userModel from "../models/users.model.js";
 import dotenv from "dotenv";
 
 dotenv.config();
