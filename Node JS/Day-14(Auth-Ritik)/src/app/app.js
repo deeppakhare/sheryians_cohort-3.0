53480ed94197541681ec1130e6c5dcd92cb75713
justforit4102 <deeppakhare19@gmail.com>
2026-09-05 21:37:45 +0530

    creating controller

--- Node JS/Day-14(Auth-Ritik)/src/app/app.js
+++ Node JS/Day-14(Auth-Ritik)/src/app/app.js
@@ -1,6 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
+import userroute from '../routes/user.route.js'
 dotenv.config()
 let app = express()
+
+app.use('/api/auth',userroute)
 
 export default app
---
+++ Node JS/Day-14(Auth-Ritik)/src/controllers/register.controller.js
@@ -0,0 +1,13 @@
+import userModel from "../models/user.model";
+
+export const registerController = async (req, res) => {
+    try {
+        
+    } catch (error) {
+        req.route(200).json({
+            message:"Registered successfully"
+            
+        })
+        
+    }
+}
