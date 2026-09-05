da0845046d8c318698f03428c23de59fa24b49a8
justforit4102 <deeppakhare19@gmail.com>
2026-09-05 21:20:35 +0530

    connected db

--- Node JS/Day-14(Auth-Ritik)/src/app/app.js
+++ Node JS/Day-14(Auth-Ritik)/src/app/app.js
@@ -1,5 +1,6 @@
 import express from "express";
-
+import dotenv from "dotenv";
+dotenv.config()
 let app = express()
 
 export default app
--- Node JS/Day-14(Auth-Ritik)/src/server.js
+++ Node JS/Day-14(Auth-Ritik)/src/server.js
@@ -1,4 +1,6 @@
 import app from "./app/app.js";
+import { connectDB } from "./config/db.js";
+connectDB();
 
 app.listen(3000,() => {
     console.log("servers is running on 3000");
