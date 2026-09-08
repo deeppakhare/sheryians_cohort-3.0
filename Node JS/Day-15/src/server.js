a4935277f4ab5b1a5d38c4a6328847ad2820a4ea
justforit4102 <deeppakhare19@gmail.com>
2026-09-08 20:47:13 +0530

    day 15 auth

--- Node JS/Day-15/src/server.js
+++ Node JS/Day-15/src/server.js
@@ -1,1 +1,8 @@
-console.log("Access tokern and refredh tokern");
+import app from './app/app.js'
+import { connectToDB } from './config/db.js';
+
+connectToDB()
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+})
