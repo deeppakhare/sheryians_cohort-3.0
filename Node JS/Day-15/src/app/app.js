d4bcf42aba2fe8bbe50454a2e6330a3f86a01e57
justforit4102 <deeppakhare19@gmail.com>
2026-09-09 23:45:52 +0530

    frontend

--- Node JS/Day-15/src/app/app.js
+++ Node JS/Day-15/src/app/app.js
@@ -1,9 +1,14 @@
-import express from "express"
+import express from "express";
+import authRoutes from "../routes/auth.routes.js";
+import cookieParser from "cookie-parser";
 
-const app = express()
+
+const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 
-app.post('/create',loginController)
 
-export default app
+app.use("/api/auth", authRoutes);
+
+export default app;
